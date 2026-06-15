4689691a7776e16a7f50a023027a6ecddee79b8a
Yoon JiHo <dbswlgh0627@gamil.com>
2026-06-15 17:56:35 +0900

    feat: 로그 출력 추가

---
+++ src/main/java/com/example/demo/Config/LoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.demo.Config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements  HandlerInterceptor{
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        String method = request.getMethod();
+        String url = request.getRequestURI();
+
+        logger.info("[API 요청 수행 완료] method={} url={} elapsedTime={}ms", method, url, elapsedTime);
+    }
+}
